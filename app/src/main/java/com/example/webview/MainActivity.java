69cfb17de6e26e2af6bb5c264d2bf8019bd88bf0
Mikhail Murtazaliev <vlga2m@gmail.com>
2020-06-23 16:01:33 +0400

    Initial commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView browser=findViewById(R.id.webBrowser);
+        browser.loadUrl("http://metanit.com");
+    }
+}
